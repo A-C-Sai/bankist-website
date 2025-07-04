bc9dab5bce62abe3beee5ae86e7664623ae2e261
Sai C Akella <sai.c.akella@gmail.com>
2025-07-05 05:48:34 +1000

    bankist

--- script.js
+++ script.js
@@ -7,8 +7,13 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const allSections = document.querySelectorAll('.section');
+const header = document.querySelector('.header');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.getElementById('section--1');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +23,11 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+// for (let i = 0; i < btnsOpenModal.length; i++) btnsOpenModal[i].addEventListener('click', openModal);
+// btnOpenModal is a NodeList NOT an Array but it has the forEach Method
+btnsOpenModal.forEach(function (btn) {
+  btn.addEventListener('click', openModal);
+});
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -28,4 +36,28 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
+});
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.innerHTML = `We use cookies for imporved functionality and analytics. <button class='btn btn--close-cookie'>Got it!</button>`;
+header.append(message);
+
+document.querySelector('.btn--close-cookie').addEventListener('click', function () {
+  // message.parentElement.removeChild(message);
+  message.remove();
+});
+
+message.style.backgroundColor = '#37383d';
+message.style.width = '100vw';
+message.style.height = Number.parseFloat(getComputedStyle(message).height) + 30 + 'px';
+
+btnScrollTo.addEventListener('click', function (e) {
+  // window.scrollTo(0, window.scrollY + section1.getBoundingClientRect().y);
+  // window.scrollTo({
+  //   left: 0,
+  //   top: window.scrollY + section1.getBoundingClientRect().y,
+  //   behavior: 'smooth',
+  // });
+  section1.scrollIntoView({ behavior: 'smooth' });
 });
